eefa9c4a9d2501c1ac7f3646da0bcb53ddb245e9
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-16 22:56:07 +0100

    draw water and textured island

--- main.js
+++ main.js
@@ -1,60 +1,57 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 let width = window.innerWidth, height = window.innerHeight;
 let canvas = document.getElementById("c");
 
-
 // init
 
 const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-
-const material = new THREE.MeshNormalMaterial();
-
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+
 renderer.setSize(width, height);
-renderer.setClearColor(new THREE.Color(0xffffff)); // hex for white
+renderer.setClearColor(new THREE.Color(0x3DABFF))
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-// animation
-//camera.position.set(new THREE.Vector3(4.5293, -7.8311, 0.61847));
-camera.position.z = 1;
-//camera.rotation.set(new THREE.Vector3(87.353, -0.64506, 13.816));
-camera.lookAt(new THREE.Vector3(0, 0, 0))
+// set up default camera state
+camera.position.set(2.2, 0.8152618946686154, 1.9230511497018643);
 
-camera.position.set(2.6547585959750655, 0.8152618946686154, 2.9230511497018643);
-//camera.rotation.set(
+// set default camera rotation
 camera.rotation.x = -0.08751335700035043;
 camera.rotation.y = 0.2524313075800641;
 camera.rotation.z = 0.021909707661728214;
 
-const loader = new OBJLoader();
-let object;
-loader.load('3d/island.obj', function (gltf) {
-    console.log(gltf);
-    object = gltf.scene;
-    scene.add(gltf);
-},
-    undefined,
-    function (error) {
-        console.error(error);
-    }
-);
+async function loadModel(name) {
+    const mtlLoader = new MTLLoader().setPath('3d/');
+    const materials = await mtlLoader.loadAsync(name + '.mtl');
+    materials.preload();
+
+    const objLoader = new OBJLoader().setPath('3d/');
+    objLoader.setMaterials(materials);
+
+    return await objLoader.loadAsync(name + '.obj')
+}
+
+let island = await loadModel("island");
+scene.add(island);
+let water = await loadModel("water");
+water.position.y = 0.125;
+scene.add(water);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 5);
+scene.add(ambientLight);
+
+const hemiLight = new THREE.HemisphereLight(0x0000ff, 0x00ff00, 0.6);
+scene.add(hemiLight);
+
 //const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate(time) {
     //controls.update();
-    console.log(camera.rotation);
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
     renderer.render(scene, camera);
 }
 
