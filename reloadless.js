7dccb858a5e6b2c4d07bc213c484b57822c14bd0
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-31 01:24:10 +0200

    implement reloadless

---
+++ reloadless.js
@@ -0,0 +1,54 @@
+// used to track which link was last pressed.
+// if you click one link, then another before the first finishes loading,
+// this variable ensures you will only be sent to the second link.
+lastClickedLink = undefined;
+
+function newPageClickHook(newText) {
+    var doc = new DOMParser().parseFromString(newText, "text/html");
+    document.getElementById("varyingContents").innerHTML = doc.getElementById("varyingContents").innerHTML;
+    setUpLinks();
+}
+
+originalState = undefined;
+
+function onPress(event) {
+    event.preventDefault();
+    let url = this.getAttribute("href");
+    lastClickedLink = url;
+    fetch(url).then((response) => {
+        response.text().then((text) => {
+            if (url != lastClickedLink) {
+                return;
+            }
+            if (window.history.state == null) {
+                originalState = { html: document.documentElement.innerHTML };
+            }
+            newPageClickHook(text);
+            window.history.pushState({ html: text }, "", url);
+        })
+    })
+}
+
+window.addEventListener("popstate", (e) => {
+    if (e.state && e.state.html) {
+        if (originalState == undefined) {
+            originalState = { html: document.documentElement.innerHTML };
+        }
+        newPageClickHook(e.state.html);
+    } else if (originalState) {
+        newPageClickHook(originalState.html);
+    }
+});
+
+
+function setUpLinks() {
+    let links = document.querySelectorAll("a[reloadless]:not([reloadless='tracked'])");
+    for (let link of links) {
+        link.setAttribute("reloadless", "tracked");
+        link.addEventListener("click", onPress.bind(link));
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setUpLinks();
+})
